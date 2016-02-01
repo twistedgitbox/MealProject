1c10ad48faea54a4e2df728f83624856d0e17505
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-02-01 14:08:41 -0800

    Added update_person_params to OrgPeopleController

--- app/controllers/org_people_controller.rb
+++ app/controllers/org_people_controller.rb
@@ -5,4 +5,14 @@ class OrgPeopleController < ApplicationController
     @contactInfo[:email] = current_org_person.email
     @person.org_contacts.build(@contactInfo.attributes)
   end
+
+  def update
+
+  end
+
+  private
+
+  def update_person_params
+    params.require(:org_person).permit(org_contacts_attributes: [:address1, :address2, :city, {typ_countries: :id}, {typ_regions: :id}, {org_company: :id}, :postal_code, :email, :business_number, :cell_number, :cell_number, :org_person_id, :avatar])
+  end
 end
