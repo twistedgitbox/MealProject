2de316a801a66852484cce690c6806ee619bc93c
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-02-01 10:40:52 -0800

    Added org_people edit view + edit controller

--- app/controllers/org_people_controller.rb
+++ app/controllers/org_people_controller.rb
@@ -1,2 +1,8 @@
 class OrgPeopleController < ApplicationController
+  def edit
+    @person = OrgPerson.find(params[:id])
+    @contactinfo = OrgContact.find_or_initialize_by(org_person_id: params[:id])
+    @contactInfo[:email] = current_org_person.email
+    @person.org_contacts.build(@contactInfo.attributes)
+  end
 end
