2eb3b9fafe0232577946d4ba476e837b2979bf1d
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-02-01 15:40:21 -0800

    Added Contact model validations and error handling

--- app/models/org_contact.rb
+++ app/models/org_contact.rb
@@ -4,4 +4,16 @@ class OrgContact < ActiveRecord::Base
   belongs_to :typ_contact, foreign_key: "typ_contact_id"
   belongs_to :typ_country, foreign_key: "typ_country_id"
   belongs_to :typ_region, foreign_key: "typ_region_id"
+  validates :typ_country, presence: true
+  validates :typ_region, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :postal_code, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :business_number, presence: true, numericality: true, length: {minimum: 10, maximum: 15}
+  validates :cell_number, numericality: true, length: {minimum: 10, maximum: 15}, allow_blank: true
+
+
 end
+
