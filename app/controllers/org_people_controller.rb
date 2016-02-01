07ac22d2c00f6b594b53db1c8d6fe1dfae67b2dd
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-02-01 12:03:36 -0800

    Added javascript and css for org_people

--- app/controllers/org_people_controller.rb
+++ app/controllers/org_people_controller.rb
@@ -1,7 +1,7 @@
 class OrgPeopleController < ApplicationController
   def edit
     @person = OrgPerson.find(params[:id])
-    @contactinfo = OrgContact.find_or_initialize_by(org_person_id: params[:id])
+    @contactInfo = OrgContact.find_or_initialize_by(org_person_id: params[:id])
     @contactInfo[:email] = current_org_person.email
     @person.org_contacts.build(@contactInfo.attributes)
   end
--- app/models/org_person.rb
+++ app/models/org_person.rb
@@ -1,7 +1,7 @@
 class OrgPerson < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :org_contacts
   belongs_to :org_company, foreign_key: "org_company_id"
