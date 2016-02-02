a8154fd467512548f335f010ab6def5d7b8f4261
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-02-01 16:59:05 -0800

    add new view for org_companies

--- app/controllers/org_companies_controller.rb
+++ app/controllers/org_companies_controller.rb
@@ -1,2 +1,22 @@
 class OrgCompaniesController < ApplicationController
+  before_action :signed_in_user, only: [:new]
+
+  def new
+    signed_in_user #Be sure the user is signed in before he can create the company
+    @company = OrgCompany.new #Make new company object
+    @contactInfo = OrgContact.new.attributes #Make new contact object w empty attributes
+    @company.org_contacts_build.(@contactInfo)
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, flash: {warning: "Please sign in."}
+    end
+  end
+
+
+
 end
